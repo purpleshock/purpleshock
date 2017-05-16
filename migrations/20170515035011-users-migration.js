748177bf4b484e47d7df39590982309126b694a2
Eric Tsai <eric.tsai@tenmax.io>
2017-05-16 12:43:42 +0800

    Add mailIdentity to users table migration

--- migrations/20170515035011-users-migration.js
+++ migrations/20170515035011-users-migration.js
@@ -13,8 +13,31 @@ module.exports = {
         type: Sequelize.DATE
       }
     })
+    await queryInterface.createTable('mailIdentities', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      mail: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    })
   },
   async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('mailIdentities')
     await queryInterface.dropTable('users')
   }
 }
