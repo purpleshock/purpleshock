e15a460c5449e9edd78b78376114d66c3ff2a004
ericyan <eric7578@gmail.com>
2017-05-22 23:26:29 +0800

    Add player/wallets database schema

---
+++ api/migrations/20170515035011-create-players.js
@@ -0,0 +1,58 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('players', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      loginAt: {
+        type: Sequelize.DATE
+      }
+    })
+    await queryInterface.createTable('mailIdentity', {
+      mail: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'players',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      }
+    })
+    await queryInterface.createTable('facebookIdentity', {
+      facebookId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'players',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('mailIdentity')
+    await queryInterface.dropTable('facebookIdentity')
+    await queryInterface.dropTable('players')
+  }
+}
---
+++ api/migrations/20170516044513-create-wallets.js
@@ -0,0 +1,74 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('wallets', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'players',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.INTEGER(1),
+        allowNull: false
+      },
+      credit: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    })
+    await queryInterface.createTable('walletTransfers', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'players',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      },
+      srcId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'wallets',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      },
+      destId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'wallets',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        allowNull: false
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('walletTransfers')
+    await queryInterface.dropTable('wallets')
+  }
+}
