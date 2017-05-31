5c3377270dd2470562525f9964a1a23dae13a4ba
Eric Tsai <eric.tsai@tenmax.io>
2017-05-31 13:34:36 +0800

    Use instanceMethods to replace prototype

--- models/Admin.js
+++ models/Admin.js
@@ -1,36 +1,32 @@
 const bcrypt = require('bcrypt')
 
-module.exports = (sequelize, DataTypes) => {
-  const Admin = sequelize.define('Admin', {
-    createdAt: {
-      type: DataTypes.DATE
+module.exports = (sequelize, DataTypes) => sequelize.define('Admin', {
+  createdAt: {
+    type: DataTypes.DATE
+  },
+  loginAt: {
+    type: DataTypes.DATE
+  },
+  mail: {
+    type: DataTypes.STRING
+  },
+  password: {
+    type: DataTypes.STRING
+  }
+}, {
+  tableName: 'admins',
+  timestamps: false,
+  instanceMethods: {
+    setPlainPassword (plain) {
+      return bcrypt.genSalt(10)
+        .then(salt => bcrypt.hash(plain, salt))
+        .then(hash => {
+          this.password = hash
+          return true
+        })
     },
-    loginAt: {
-      type: DataTypes.DATE
-    },
-    mail: {
-      type: DataTypes.STRING
-    },
-    password: {
-      type: DataTypes.STRING
+    comparePlainPassword (plain) {
+      return bcrypt.compare(plain, this.password)
     }
-  }, {
-    tableName: 'admins',
-    timestamps: false
-  })
-
-  Admin.Instance.prototype.setPlainPassword = function (plain) {
-    return bcrypt.genSalt(10)
-      .then(salt => bcrypt.hash(plain, salt))
-      .then(hash => {
-        this.password = hash
-        return true
-      })
   }
-
-  Admin.Instance.prototype.comparePlainPassword = function (plain) {
-    return bcrypt.compare(plain, this.password)
-  }
-
-  return Admin
-}
+})
