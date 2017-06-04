db88a1efbc0023296f56ea3c50e5c94a00b76af3
ericyan <eric7578@gmail.com>
2017-06-04 09:26:12 +0800

    Fix infinite loop when ap is offline

--- ui/admin/src/router/hooks.js
+++ ui/admin/src/router/hooks.js
@@ -1,6 +1,4 @@
 import { me, localStorage } from '../services'
-import store from '../store'
-import { ON_GET_INFO } from '../store/modules/user'
 
 export function needAnnoymous (to, from, next) {
   const jwt = localStorage.getJWT()
@@ -17,15 +15,21 @@ export async function needLogin (to, from, next) {
     return next('/')
   }
 
-  let userInfo
+  let userInfo, respErr
   try {
     userInfo = await me.getUserInfo()
   } catch (err) {
-    userInfo = false
+    respErr = err
+  }
+
+  // off-line or internal error
+  if (respErr) {
+    if (!respErr.hasOwnProperty('status') || respErr.status === 500) {
+      return console.error('Oops. Something happens')
+    }
   }
 
   if (userInfo) {
-    await store.commit(ON_GET_INFO, userInfo)
     return next()
   } else {
     return next('/')
