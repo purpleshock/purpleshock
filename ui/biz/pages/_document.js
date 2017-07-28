2e238fb86a5d2d3cc41c9692bcb37361f02089f6
ericyan <eric7578@gmail.com>
2017-07-28 21:53:19 +0800

    Add styled-components

---
+++ ui/biz/pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class RootDocument extends Document {
+  render () {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+
+    return (
+      <html>
+        <Head>
+          <title>Biz - purpleshock</title>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css" />
+          {styleTags}
+        </Head>
+        <body>
+          <div className='root'>
+            {main}
+          </div>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
