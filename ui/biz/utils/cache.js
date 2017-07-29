742459667463e666a14c51d38bbbb25ca0b9f897
ericyan <eric7578@gmail.com>
2017-07-29 11:11:05 +0800

    Stop caching when in development

--- ui/biz/utils/cache.js
+++ ui/biz/utils/cache.js
@@ -1,6 +1,7 @@
 const LRUCache = require('lru-cache')
 const logger = require('./logger')
 
+const usingCache = process.env.NODE_ENV === 'production'
 const ssrCache = new LRUCache({
   max: 100,
   maxAge: 1000 * 60 * 60
@@ -13,19 +14,28 @@ const defaultOption = {
 }
 
 module.exports = (next, option = defaultOption) => async (req, res, path, vars) => {
-  const key = option.cacheKey(req)
-
-  if (ssrCache.has(key)) {
-    logger(`Hitting cache ${key}`)
-    return res.send(ssrCache.get(key))
+  try {
+    if (usingCache) {
+      await useCacheRenderer(req, res, path, vars)
+    } else {
+      const html = await next.renderToHTML(req, res, path, vars)
+      res.send(html)
+    }
+  } catch (err) {
+    next.renderError(err, req, res, path, vars)
   }
 
-  try {
+  async function useCacheRenderer (req, res, path, vars) {
+    const key = option.cacheKey(req)
+
+    if (ssrCache.has(key)) {
+      logger(`Hitting cache ${key}`)
+      return res.send(ssrCache.get(key))
+    }
+
     logger(`Missing cache ${key}`)
     const html = await next.renderToHTML(req, res, path, vars)
     ssrCache.set(key, html)
     res.send(html)
-  } catch (err) {
-    next.renderError(err, req, res, path, vars)
   }
 }
