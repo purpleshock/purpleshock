abba592358e6c476b67b4ecd21888e713c8cfa85
ericyan <eric7578@gmail.com>
2017-08-02 20:44:39 +0800

    Use wrapper to refactor admins middleware

--- middlewares/v1/admins.js
+++ middlewares/v1/admins.js
@@ -1,36 +1,47 @@
 const express = require('express')
 const httpError = require('../../utils/httpError')
-const wrap = require('../wrap')
-const { registration, finder, token, permission } = require('../../services')
-const { TYPE_MAIL_PWD, validate } = require('../../utils/validators')
+const wrapper = require('../wrapper')
+const registration = require('../../services/registration')
+const finder = require('../../services/finder')
+const token = require('../../services/token')
+const permission = require('../../services/permission')
+const formatter = require('./formatters/admins')
 
 const admins = express.Router()
 
-admins.post('/', wrap(async (req, res) => {
-  const { mail, password } = await validate(req.body, TYPE_MAIL_PWD)
-  const admin = await registration.registerAdmin(mail, password)
-  const scopes = await permission.getAdminScopes(admin)
-  const adminToken = await token.grantAdmin(admin, scopes)
-  res.json({
-    token: adminToken
-  })
+admins.post('/', wrapper({
+  body: formatter.registerAdminBody,
+  response: formatter.registerAdminResponse,
+  async handler (req, res) {
+    const { mail, password } = req.body
+    const admin = await registration.registerAdmin(mail, password)
+    const scopes = await permission.getAdminScopes(admin)
+    const adminToken = await token.grantAdmin(admin, scopes)
+    return {
+      token: adminToken
+    }
+  }
 }))
 
-admins.post('/session', wrap(async (req, res) => {
-  const { mail, password } = await validate(req.body, TYPE_MAIL_PWD)
-  const admin = await finder.findAdminByMail(mail, password)
-  if (!admin) {
-    throw httpError(404)
+admins.post('/session', wrapper({
+  body: formatter.exchangeTokenBody,
+  response: formatter.exchangeTokenResponse,
+  async handler (req, res) {
+    const { mail, password } = req.body
+    const admin = await finder.findAdminByMail(mail, password)
+    if (!admin) {
+      throw httpError(404)
+    }
+    const scopes = await permission.getAdminScopes(admin)
+    const accessToken = await token.grantAdmin(admin, scopes)
+    return {
+      token: accessToken
+    }
   }
-  const scopes = await permission.getAdminScopes(admin)
-  const accessToken = await token.grantAdmin(admin, scopes)
-  res.json({
-    token: accessToken
-  })
 }))
 
 admins.get('/token', permission.getCheckTokenMiddleware(), (req, res) => {
-  res.end()
+  res.end('OK')
 })
 
 module.exports = admins
