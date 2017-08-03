8dedd0c781fb4f6d411f5b8ce0a02695bf48e112
Eric Tsai <eric.tsai@tenmax.io>
2017-08-03 17:19:57 +0800

    Add sub router for mapping voucher to code

---
+++ middlewares/v1/vouchers/code.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const wrapper = require('../../wrapper')
+const voucherFinder = require('../../../services/voucherFinder')
+const batchFinder = require('../../../services/batchFinder')
+const permission = require('../../../services/permission')
+const formatters = require('../formatters/vouchers')
+const httpError = require('../../../utils/httpError')
+
+const router = express.Router({
+  mergeParams: true
+})
+
+router.use(permission.getCheckScopesMiddleware(['vouchers.find']), async (req, res, next) => {
+  const voucher = await voucherFinder.findByCode(req.params.code)
+  if (voucher) {
+    res.locals.voucher = voucher
+    next()
+  } else {
+    return next(httpError(404))
+  }
+})
+
+router.get('/', wrapper({
+  async handler(req, res) {
+    const { voucher } = res.locals
+    const batch = await batchFinder.findById(voucher.batchId)
+    return Object.assign(voucher, {
+      batch: batch.code
+    })
+  }
+}))
+
+module.exports = router
