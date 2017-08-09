9d8b90265f6c39cfd15509348b59f100c6bbf911
ericyan <eric7578@gmail.com>
2017-08-09 23:10:15 +0800

    Remove unnecessary component in voucher-management

--- ui/biz/pages/voucher-management.js
+++ ui/biz/pages/voucher-management.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
-import Router from 'next/router'
 import withRedux from 'next-redux-wrapper'
 import Authenticated from '../components/Authenticated'
 import Layout from '../components/Layout'
@@ -18,24 +17,6 @@ export default class VoucherManagement extends PureComponent {
     onCreateVouchers: PropTypes.func.isRequired
   }
 
-  static async getInitialProps ({ isServer, store, query }) {
-    const prefetch = query && query.page && (query.validAt || query.expiredAt)
-    const props = {
-      prefetch
-    }
-
-    if (prefetch) {
-      const { validAt, expiredAt, page } = query
-      if (isServer) {
-        await store.dispatch(searchHistory(validAt, expiredAt, page))
-      } else {
-        store.dispatch(searchHistory(validAt, expiredAt, page))
-      }
-    }
-
-    return props
-  }
-
   render () {
     return (
       <Layout>
@@ -43,19 +24,6 @@ export default class VoucherManagement extends PureComponent {
         <FindVoucher />
       </Layout>
     )
-  }
-
-  onSearchHistory = (validAt, expiredAt) => {
-    if (validAt || expiredAt) {
-      Router.push({
-        pathname: '/voucher-management',
-        query: {
-          page: 1,
-          validAt: validAt && validAt.unix(),
-          expiredAt: expiredAt && expiredAt.unix()
-        }
-      })
-    }
   }
 }
 
@@ -69,8 +37,7 @@ function mapStateToProps (state) {
 function mapDispatchToProps (dispatch) {
   return {
     ...bindActionCreators({
-      onCreateVouchers: createVouchers,
-      onSearchHistory: searchHistory
+      onCreateVouchers: createVouchers
     }, dispatch)
   }
 }
