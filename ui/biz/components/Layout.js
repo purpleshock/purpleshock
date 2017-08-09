a440be64ee9c0c192adb1f834b89fe2e9bde87b3
ericyan <eric7578@gmail.com>
2017-08-09 22:20:38 +0800

    Remove unnecessary Components

--- ui/biz/components/Layout.js
+++ ui/biz/components/Layout.js
@@ -1,7 +1,14 @@
 import React, { PureComponent } from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { Container, Menu, SideBar, Icon } from 'semantic-ui-react'
-import Link from './Link'
+import { Container, Menu, SideBar } from 'semantic-ui-react'
+import Link from 'next/link'
+
+const MenuLink = styled.a`
+  align-items: center;
+  display: flex;
+  padding: 10px;
+`
 
 export default class Layout extends PureComponent {
   static propTypes = {
@@ -12,13 +19,15 @@ export default class Layout extends PureComponent {
     return (
       <div>
         <Menu attached='top'>
-          <Menu.Item as={Link} href='/dashboard'>
-            <Icon name='dashboard' />
-            Dashboard
+          <Menu.Item as={Link} href='/dashboard' passHref>
+            <MenuLink>
+              Dashboard
+            </MenuLink>
           </Menu.Item>
-          <Menu.Item as={Link} href='/voucher-management'>
-            <Icon name='tasks' />
-            Management
+          <Menu.Item as={Link} href='/voucher-management' passHref>
+            <MenuLink>
+              Management
+            </MenuLink>
           </Menu.Item>
         </Menu>
         <Container>
