ce655c4230f72c557d054f55bf66e8bee7cdd9f2
Eric Tsai <eric.tsai@tenmax.io>
2017-09-06 14:06:34 +0800

    Initial commit

---
+++ biz/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import document from 'global/document'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <h1>BIZ</h1>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
---
+++ biz/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+}
